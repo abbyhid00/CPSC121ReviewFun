55cce8aa5fa8714335c98992f7e7c15883e8326c
Abby Hidalgo <abbyhidalgo03@gmail.com>
2023-01-26 22:56:42 +0000

    initial

---
+++ vecfuncs.cpp
@@ -0,0 +1,24 @@
+#include "vecfuncs.h"
+void fillVector(vector<int>& vec){
+  int num;
+  for (int i = 0; i < 5; i++){
+    cout << "Enter an int: ";
+    cin >> num;
+    vec.push_back(num);
+  }
+  }
+
+void findExtremes(vector<int>& vec, int& min, int& max){
+// assumes vec has at least one element
+  min = max = vec.at(0);
+  for (int i = 0; i < vec.size(); i++){
+    if (vec.at(i) < min){
+      //new min
+      min = vec.at(i);   
+    }
+    if (vec.at(i) > max){
+      max = vec.at(i);
+    }
+  }
+}
+
---
+++ vecfuncs.h
@@ -0,0 +1,13 @@
+// header file guard code prevents the problem 
+// of double inclusion of a header file 
+// for more details
+#ifndef VECFUNCS_H
+#define VECFUNCS_H
+#include <iostream>
+#include <vector>
+
+using namespace std;
+void fillVector(vector<int>&); // prototype
+void findExtremes(vector<int>&, int&, int&);
+
+#endif
